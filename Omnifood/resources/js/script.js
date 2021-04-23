ad07e88dee81f0b7e52253cf10f35ec50678e507
Emmanuel Appiah <82161457+MrAppiah@users.noreply.github.com>
2021-04-23 18:58:06 +0100

    Add files via upload

--- Omnifood/resources/js/script.js
+++ Omnifood/resources/js/script.js
@@ -1,45 +1,51 @@
 /* global $ */
 /* eslint-env jquery */
-$('document').ready(function() {
+$("document").ready(function () {
+	/* For the sticky navigation */
+	$(".js--section-features").waypoint(
+		function (direction) {
+			if (direction == "down") {
+				$("nav").addClass("sticky");
+			} else {
+				$("nav").removeClass("sticky");
+			}
+		},
+		{
+			offset: "60px;",
+		}
+	);
 
-    /* For the sticky navigation */
-    $('.js--section-features').waypoint(function(direction) {
-    if (direction == "down") {
-       $('nav').addClass('sticky');
-    } else {
-        $('nav').removeClass('sticky');
-        }
-    }, {
-        offset: '60px;'
-  });
-    
-    
-    /* Scroll on buttons */
-    $('.js--scroll-to-plans').click(function (){
-        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 500);
-    });
-    
-    $('.js--scroll-to-start').click(function (){
-        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 500);
-    });
-    
-    /* Navigation scroll completed in css */
+	/* Scroll on buttons */
+	$(".js--scroll-to-plans").click(function () {
+		$("html, body").animate(
+			{ scrollTop: $(".js--section-plans").offset().top },
+			500
+		);
+	});
 
-    /* Animations on scroll completed in html/css */
+	$(".js--scroll-to-start").click(function () {
+		$("html, body").animate(
+			{ scrollTop: $(".js--section-features").offset().top },
+			500
+		);
+	});
 
-    /* Mobile nav */
-    $('.js--nav-icon').click(function() {
-        var nav = $('.js--main-nav');
-        var icon = $('.js--nav-icon i');
-        
-        nav.slideToggle(200);
-        if (icon.hasClass('ion-navicon-round')) {
-            icon.addClass('ion-close-round');
-            icon.removeClass('ion-navicon-round')
-        } else {
-            icon.addClass('ion-navicon-round');
-            icon.removeClass('ion-close-round');
-        }
-    });
+	/* Navigation scroll completed in css */
 
-});   
+	/* Animations on scroll completed in html/css */
+
+	/* Mobile nav */
+	$(".js--nav-icon").click(function () {
+		var nav = $(".js--main-nav");
+		var icon = $(".js--nav-icon i");
+
+		nav.slideToggle(200);
+		if (icon.hasClass("ion-navicon-round")) {
+			icon.addClass("ion-close-round");
+			icon.removeClass("ion-navicon-round");
+		} else {
+			icon.addClass("ion-navicon-round");
+			icon.removeClass("ion-close-round");
+		}
+	});
+});
